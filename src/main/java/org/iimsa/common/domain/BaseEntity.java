8c25045a7519417678c4c4649cc8f9100f2a6474
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-04-01 16:02:00 +0900

    chore: BaseEntity 단일화

--- src/main/java/org/iimsa/common/domain/BaseEntity.java
+++ src/main/java/org/iimsa/common/domain/BaseEntity.java
@@ -7,9 +7,13 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.iimsa.common.util.SecurityUtil;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.StringUtils;
 
 @Getter
 @MappedSuperclass
@@ -25,4 +29,28 @@ public abstract class BaseEntity {
     protected LocalDateTime modifiedAt;
 
     protected LocalDateTime deletedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    protected String createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    protected String modifiedBy;
+
+    @Column(insertable = false)
+    protected String deletedBy;
+
+    protected void delete(String deletedBy) {
+        // 이미 삭제된 경우 중복 처리 방지
+        if (this.deletedAt != null) {
+            return;
+        }
+
+        this.deletedBy = StringUtils.hasText(deletedBy)
+                ? deletedBy
+                : SecurityUtil.getCurrentUsername().orElse("SYSTEM");
+
+        this.deletedAt = LocalDateTime.now();
+    }
 }
